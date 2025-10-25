ee8b1e2e3f18bcd679ebce449876491c22827e32
Connor Quiroz <quir1869@uw.edu>
2025-10-25 12:01:28 -0700

    add assignment 3 files

--- Homework 3/QuirozC_HomeworkAssignment3/QuirozC_HomeworkAssignment3.R
+++ Homework 3/QuirozC_HomeworkAssignment3/QuirozC_HomeworkAssignment3.R
@@ -96,7 +96,7 @@ popModel <- function(s0 = 0.9,s1 = 0.95, k1_plus = 15000, fmax = 0.29, z = 2.39,
 
 
 
-a_s <- popModel(s0 = 0.9, s1 = 0.95, k1_plus = 15000, fmax = 0.29, z = 2.39, catch = data$catches)
+a_s <- popModel(s0 = 0.9, s1 = 0.95, k1_plus = 15000, fmax = 0.29, z = 2.39, catch = AllCatch)
 
 # Clean up dataset
 colnames(a_s) <- c("Age 0", "Age 1", "Age 2", "Age 3", "Age 4", "Age 5", "Age 6", "Age 7", "Age 8", "Age 9", "Age 10", "Age 11", "Age 12", "Age 13")
